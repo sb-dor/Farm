cb9089f78a6a347550d62434d5680f1878b0457b
sb-dor <avaz.shams.2002@gmail.com>
2025-07-20 22:37:34 +0500

    making assets

---
+++ assets/tiles/Bonfire_01-Sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Bonfire_01-Sheet" tilewidth="32" tileheight="32" tilecount="4" columns="4">
+ <image source="../Crawler-Pack/Environment/Structures/Stations/Bonfire/Bonfire_01-Sheet.png" width="128" height="32"/>
+</tileset>
